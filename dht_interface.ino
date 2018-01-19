8fbc0c03dcc07df5dd894274cde5abdddb179981
Anuj Jain <33128728+ianuj03@users.noreply.github.com>
2018-01-19 13:52:09 +0530

    Update dht_interface.ino

--- dht_interface.ino
+++ dht_interface.ino
@@ -11,23 +11,25 @@ void setup(){
   Serial.println("Status\tHumidity\tTemperature(C)\tTemperature(F)\tTemperature(k)");
 }
 
-float tempc,humidity;
+float tempc=0,humidity=0;
 float tempf(float tempc){
   return (tempc*(9.0/5.0)+32);//Converting to Fahrenheit
 }
 
 void loop(){
-  tempc=dht.getTemperature();//Reading temperature in Celsius
-  humidity=dht.getHumidity();//Reading Humidity in percentage
-  Serial.print(dht.getStatusString());//returns the status of the module.
-  Serial.print("\t");
-  Serial.print(humidity,2);//Print the value upto two decimal place
-  Serial.print("(%)\t\t");
-  Serial.print(tempc,2);
-  Serial.print("C\t\t");
-  Serial.print(tempf(tempc),2);
-  Serial.print("F\t\t");
-  Serial.print(tempc+273);//Adding 273 to convert into Kelvin
-  Serial.println("K");
-  delay(1000);//Delay one second
+  if(tempc!=dht.getTemperature()||humidity!=dht.getHumidity()){//Check for change.
+    tempc=dht.getTemperature();//Reading temperature in Celsius
+    humidity=dht.getHumidity();//Reading Humidity in percentage
+    Serial.print(dht.getStatusString());//returns the status of the module.
+    Serial.print("\t");
+    Serial.print(humidity,2);//Print the value upto two decimal place
+    Serial.print("(%)\t\t");
+    Serial.print(tempc,2);
+    Serial.print("C\t\t");
+    Serial.print(tempf(tempc),2);
+    Serial.print("F\t\t");
+    Serial.print(tempc+273);//Adding 273 to convert into Kelvin
+    Serial.println("K");
+    delay(1000);//Delay one second
+  }
 }

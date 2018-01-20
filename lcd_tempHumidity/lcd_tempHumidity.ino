2714832bef16a92162c5409ee6cbc01feff45e40
Anuj Jain <33128728+ianuj03@users.noreply.github.com>
2018-01-20 15:39:33 +0530

    lcd_interface_dht11
    Interfacing DHT11 with Arduino, and printing the data on the LCD screen.

---
+++ lcd_tempHumidity/lcd_tempHumidity.ino
@@ -0,0 +1,23 @@
+#include<LiquidCrystal.h>
+#include<DHT.h>//Need to download this library if not done
+DHT dht;
+LiquidCrystal lcd(8,9,2,3,4,5);//rs,en,d4,d5,d6,d7
+void setup(){
+  lcd.begin(16,2);//row,column
+  dht.setup(7);//Connecting data pin of the DHT11 to digital pin 7
+}
+float tempc=0,humidity=0;
+void loop(){
+  if(tempc!=dht.getTemperature()||humidity!=dht.getHumidity()){//Checking for the change
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Temp:");
+    lcd.print(dht.getTemperature());//Printing Temperature in Celsius
+    lcd.print("C");
+    lcd.setCursor(1,0);
+    lcd.print("Humidity:");
+    lcd.print(dht.getHumidity());//Printing Humidity in Percentage
+    lcd.print("%");
+  }
+}
+
